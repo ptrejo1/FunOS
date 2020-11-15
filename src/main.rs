6490a55350fbecf7bd116263ad2facd695359afb
Phoenix Trejo <phoenixtrejo@yahoo.com>
2020-11-14 21:58:28 -0700

    interrupts

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,14 @@ use fun_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    fun_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
